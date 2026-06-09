43c6f40b5a84cbb621bf93fd3112a80e8b040c3f
muskansharma-17 <muskansharma170202@gmail.com>
2026-06-09 18:04:53 +0530

    use environment database url

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://root:muskan%40mysql1702@localhost:3306/solarsense"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable not set")
 
 engine = create_engine(DATABASE_URL)
 
